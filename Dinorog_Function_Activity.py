81a01463e0ff8fd5fecd3077c8a62205bfe8b921
Artjohn Clark Dinorog <144906804+Deucariousity@users.noreply.github.com>
2023-12-13 21:46:10 +0800

    Update Dinorog_Function_Activity.py

--- Dinorog_Function_Activity.py
+++ Dinorog_Function_Activity.py
@@ -8,8 +8,18 @@ def same_num(num1, num2, num3):
     return result
 
 
-def same_second_num(num1, num2, num3):
+def same_two_num1(num1, num2, num3):
     result = num1 * num2 + num3
+    return result
+
+
+def same_two_num2(num1, num2, num3):
+    result = num1 * num3 + num2
+    return result
+
+
+def same_two_num3(num1, num2, num3):
+    result = num2 * num3 + num1
     return result
 
 
@@ -28,7 +38,7 @@ print("CS112: Introduction to Programming 1"
       "\nLABORATORY ACTIVITY: FUNCTIONS")
 
 print("\nGuide Rules:"
-      "\n[1] If all numbers is the same it will be multiplied."
+      "\n[1] If all numbers are the same it will be multiplied."
       "\n[2] If all numbers are different it will be added."
       "\n[3] If two numbers are the same and the other one is different,"
       "\nthe two same numbers will be multiplied and the other one is added.")
@@ -39,30 +49,22 @@ while True:
     num2 = eval(input("Enter second number: "))
     num3 = eval(input("Enter third number: "))
     num = num1, num2, num3
-    if num1 != num2 and num3:
+    if num1 != num2 != num3:
         print("Result is", diff_num(num1, num2, num3))
 
-    elif num1 == num2 and num3:
+    elif num1 == num2 == num3:
         print("Result is", same_num(num1, num2, num3))
 
     elif num1 == num2 != num3:
-        print("Result is", same_second_num(num1, num2, num3))
+        print("Result is", same_two_num1(num1, num2, num3))
 
-    elif num1 == num3 != num2:
-        print("Result is", same_second_num(num1, num2, num3))
-
-    elif num2 == num1 != num3:
-        print("Result is", same_second_num(num1, num2, num3))
+    elif num1 == num2 != num3:
+        print("Result is", same_two_num2(num1, num2, num3))
 
     elif num2 == num3 != num1:
-        print("Result is", same_second_num(num1, num2, num3))
-
-    elif num3 == num1 != num2:
-        print("Result is", same_second_num(num1, num2, num3))
-
-    elif num3 == num2 != num1:
-        print("Result is", same_second_num(num1, num2, num3))
+        print("Result is", same_two_num3(num1, num2, num3))
 
     another = get_user_input("\nTry Again? (yes, no): ")
     if another != 'yes':
         break
+
